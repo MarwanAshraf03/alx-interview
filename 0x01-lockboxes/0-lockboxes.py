aab8b8788fec19aca177e555a4cba0b2c777dfee
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-14 03:19:34 +0300

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,6 +7,8 @@ def canUnlockAll(boxes):
     ln = len(boxes) - 1
     summation = (ln/2) * (2 * 1 + (ln - 1) * 1)
     list_of_keys = boxes[0][:]
+    if summation == sum(set(list_of_keys)):
+        return True
     lcopy = list_of_keys.copy()
     for i in range(len(boxes)):
         for key in lcopy:
