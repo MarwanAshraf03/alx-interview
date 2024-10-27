4c8f45029dc186d474d75f94702fda08b8e14f71
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-27 23:39:26 +0300

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Module for logging some data to the stdout"""
+"""Module for logging some data to stdout"""
 import sys
 import re
 import signal
 
 
-def after_10(signum, frame):
-    """function to run every 10 headers or after keyinterrupt"""
+def after_10():
+    """Function to run every 10 headers or after a signal interrupt."""
     print(f"File size: {file_size}")
     for s, c in statuses.items():
         if c > 0:
             print(f"{s}: {c}")
+
+
+def handle_exit(signum, frame):
+    """Handle exit signals by running after_10 and then exiting."""
+    after_10()
+    sys.exit(0)
 
 
 if __name__ == "__main__":
@@ -23,35 +29,37 @@ if __name__ == "__main__":
         "404": 0,
         "405": 0,
         "500": 0
-        }
-    signal.signal(signal.SIGINT, after_10)
+    }
     file_size = 0
     count = 0
-    while True: 
+
+    # Set up signal handlers
+    signal.signal(signal.SIGINT, handle_exit)  # Handle Ctrl+C
+    signal.signal(signal.SIGPIPE, handle_exit)  # Handle broken pipe
+
+    while True:
         try:
             text = sys.stdin.readline()
             if not text:
-                # print("couldn't get from stdout")
-                after_10(None, None)
+                after_10()
                 break
             pattern = (
-                        r'([\d]+\.[\d]+\.[\d]+\.[\d]+) - '
-                        r'\[[\d]{4}-[\d]{2}-[\d]{2} '
-                        r'[\d]{2}:[\d]{2}:[\d]{2}\.[\d]{2,}\] '
-                        r'"GET \/projects\/260 HTTP\/1\.1" '
-                        r'[\d]{3} '
-                        r'[\d]{1,4}'
-                        )
+                r'([\d]+\.[\d]+\.[\d]+\.[\d]+) - '
+                r'\[[\d]{4}-[\d]{2}-[\d]{2} '
+                r'[\d]{2}:[\d]{2}:[\d]{2}\.[\d]{2,}\] '
+                r'"GET \/projects\/260 HTTP\/1\.1" '
+                r'[\d]{3} '
+                r'[\d]{1,4}'
+            )
             splitted = text.split()
             match = re.search(pattern, text)
             if match is not None:
                 file_size += int(splitted[-1])
                 statuses[splitted[-2]] += 1
                 if count == 10:
-                    after_10(None, None)
+                    after_10()
                     count = 0
         except KeyboardInterrupt:
-            # print("Pressed")
-            after_10(None, None)
+            handle_exit(None, None)
 
         count += 1
