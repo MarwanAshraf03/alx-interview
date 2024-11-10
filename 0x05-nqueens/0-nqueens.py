2e92047c9d5dc6908319724db461640544127974
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-11-11 00:56:32 +0200

    0*2

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """nqueens solution module"""
-from typing import Mapping
+import sys
+# n = 4
 
 
-def possible_sols(n:int, pos: tuple, SolMap: Mapping) -> Mapping:
+def possible_sols(n, pos, SolMap):
+    """adds the positions that are filled by the pos in the solMap"""
     SolMap[str(pos)] = []
     SolMap[str(pos)].append((pos[0], -1))
     SolMap[str(pos)].append((-1, pos[1]))
@@ -20,7 +22,8 @@ def possible_sols(n:int, pos: tuple, SolMap: Mapping) -> Mapping:
             SolMap[str(pos)].append((pos[0]-k, pos[1]+k))
     return SolMap
 
-def possible(pos: tuple, SolMap: Mapping):
+
+def possible(pos, SolMap):
     """returns the possible position in the row"""
     solList = [i[:] for i in SolMap.values()]
     solList = [i for j in solList for i in j]
@@ -31,43 +34,22 @@ def possible(pos: tuple, SolMap: Mapping):
     if pos in solList:
         return False
     return True
-    # for col in range(n):
-    #     if (-1, col)in solList:
-    #         continue
-    #     if (row, col) not in solList:
-    #         return (row, col)
-    # return None
 
 
 solListt = []
 
 
-def backTrack(row: int, n: int, SolMap: Mapping, sol: list):
+def backTrack(row, n, SolMap, sol):
+    """uses backtracking to find best solutions"""
     if row == n:
-        print(100 * '!')
         solListt.append(sol.copy())
-        print(solListt)
         sol.clear()
-        # SolMap = {}
-        # print(sol)
-        # print(SolMap)
         return
-    print("entered")
-    print(f"{50 * '-'}row = {row}{50 * '-'}")
-    # if len(solListt) > 0:
-    #     solListt.pop()
     for col in range(n):
-
-        # if len(solListt) == n-1 and row == 0:
-        #     print(100 * "h")
-        #     solListt.clear()
         if row == 0:
-            print(100 * "X")
             SolMap = {}
         if len(sol) == n-1 and row == 0:
-            print(100 * "h")
             sol.clear()
-        print(f"{50 * '+'} iteration: {col} in row = {row} {50 * '+'}")
         pos = (row, col)
         if str((row, col-1)) in SolMap.keys():
             keysList = list(SolMap.keys())
@@ -76,49 +58,32 @@ def backTrack(row: int, n: int, SolMap: Mapping, sol: list):
                 del SolMap[keysList[hk]]
         if (row, col-1) in sol:
             sol = sol[0:sol.index((row, col-1))]
-            # sol.remove((row, col-1))
-        print(f"before validation pos = {pos}")
-        print(f"sol = {sol}")
         if possible(pos, SolMap):
-            print(f"after validation pos = {pos}")
             SolMap = possible_sols(n, pos, SolMap)
             sol.append(pos)
-            print(f"the str(row, col-1) = {(row, col-1)} while the sol = {sol} and the condition is {(row, col-1) in sol}")
             if (row, col-1) in sol:
                 sol.remove((row, col-1))
-                print(f"removed = {(row, col-1)}")
-            print(f"the str(row, col-1) = {(row, col-1)} while the sol = {sol} and the condition is {(row, col-1) in sol}")
-            # solListt.append(pos)
-            # print(f"the str(row, col-1) = {(row, col-1)} while the solListt = {solListt} and the condition is {(row, col-1) in solListt}")
-            # if (row, col-1) in solListt:
-            #     solListt.remove((row, col-1))
-            #     print(f"popped = {(row, col-1)}")
-            # solListt.append(pos)
-            # print(f"the str(row, col-1) = {(row, col-1)} while the solListt = {solListt} and the condition is {(row, col-1) in solListt}")
             backTrack(row + 1, n, SolMap, sol)
         else:
-            # print(solListt)
-            print(SolMap)
             li = list(SolMap.keys()).copy()
             for i in li:
                 if f"({row}," in i:
                     del SolMap[i]
-                    # sol.remove(tuple(i))
-            # if len(solListt) > 0:
-            #     solListt.pop()
             continue
-    # return sol
-        # if col == n-1:
-        # if len(solListt) == n:
-        #     print(f"{50 * '*'}solListt = {solListt}{50 * '*'}")
-        #     # del SolMap[str(pos)]
-        #     return
-        #     # break
+
 
 if __name__ == "__main__":
-    print("start")
-    backTrack(0, 6, {}, [])
-    # print(f"solListt = {solListt}")
-    print("solListt = \n")
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    backTrack(0, n, {}, [])
     for s in solListt:
         print(s)
